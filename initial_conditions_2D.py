0b5f026023bcf20fcc482d3fa40bef06080c920d
Julian Schmitt <jschmitt@college.harvard.edu>
2021-12-07 23:25:13 -0500

    adds comments

--- initial_conditions_2D.py
+++ initial_conditions_2D.py
@@ -2,8 +2,10 @@ import numpy as np
 from numpy import exp
 from math import pi
 import matplotlib.pyplot as plt
-from numpy.core.numeric import convolve
+from random import seed
 
+seed(205) # set seed 
+# define constants and initialize grid
 eps = 0.05
 res=1000
 dim = int(np.ceil(res*(1+2*eps)))
@@ -11,21 +13,27 @@ phi = np.random.rand(dim,dim)
 
 
 def w_matrix(eps, res):
-    eps_res = 2*int(eps*res)
+    """Defines exponential decay matrix based on grid resolution (res) and communication length eps"""
+    eps_res = 2*int(eps*res) # eps on each side
     weight_matrix = np.zeros((eps_res, eps_res))
-    i, j = np.floor(weight_matrix.shape[0])/2, np.floor(weight_matrix.shape[0])/2
+    i, j = np.floor(weight_matrix.shape[0])/2, np.floor(weight_matrix.shape[0])/2 # index of middle gridpoint
+    # iterate through gridpoints to determine weighting by distance
     for k in range(weight_matrix.shape[0]):
         for l in range(weight_matrix.shape[1]):
-            weight_matrix[k,l] = np.exp(- (((i-k)/res)**2+((j-l)/res)**2) /(2*np.pi*eps**2))
+            # division (can be adjusted) gives scale of how fast weighting decreases
+            weight_matrix[k,l] = np.exp(- (((i-k)/res)**2+((j-l)/res)**2) /(2*np.pi*eps**2)) 
     return weight_matrix
 weight_matrix = w_matrix(eps, res)
 
 def convolve_matrix(phi, eps, res):
+    """Smoothing function: applies weight matrix to all gridpoints in phi matrix"""
     eps_res = int(eps*res)
     smoothed = np.zeros((res, res))
     for i in range(eps_res, smoothed.shape[0]+eps_res):
         for j in range(eps_res,smoothed.shape[0]+eps_res):
+            # index out communication area
             communication_area = phi[i-eps_res:i+eps_res, j-eps_res: j+eps_res]
+            # elementwise sum of weights and communication points
             smoothed[i-eps_res,j-eps_res] = np.sum(np.multiply(communication_area, weight_matrix))/(eps_res*2+1)**2
     return smoothed
 
