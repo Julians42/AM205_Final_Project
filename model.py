9f1982fd12e64d2be611608f32cf08676487dce0
Julian Schmitt <jschmitt@college.harvard.edu>
2021-12-10 22:44:00 -0500

    Buggy update rho/density

--- model.py
+++ model.py
@@ -1,14 +1,65 @@
 from utils import *
+# from initial_conditions_2D import eps, res, dim, phi, weight_matrix, convolve_matrix
+import matplotlib.pyplot as plt
+from math import pi
 
+res = 50
+eps =0.2
+dim = int(np.ceil(res*(1+2*eps)))
 # Constants
 t_step = 0.05
+dx = 1/res; dy = 1/res # assume grid is total size 1
+weight_matrix = w_matrix(eps, res)
+phi0 = convolve_matrix(np.random.rand(dim, dim), eps, res)
 
 
 # generate initial conditions
-phi0 = np.genfromtxt("data/initial_conditions.txt")
+#phi0 = convolve_matrix(phi, eps, res)
+rho0 = np.zeros(phi0.shape)
+q0_x = np.zeros((phi0.shape[0]+1, phi0.shape[1]))
 
-vfluid0 = np.zeros(phi0.shape)
-fdensity0 = np.zeros(phi0.shape)
-press0 = np.zeros((phi0.shape[0]+1, phi0.shape[1]+1))
+# fluid forcing on the x_direction of q
+additive_q = fluid_forcing_x(q0_x.shape, t_step*1, 1, len_force=int(res/4), start=int(res/2)) # fluid forcing on q
 
-fluid_forcing(phi0.shape, t_step*1, 1)
+# we now derive an update rule for q based on phi and rho
+def update_rho(phi_mat, rho_mat, Kappa, add_qx_mat, add_qy_mat, dx =dx):
+    """Updates pressure matrix by considering flux"""
+    # have one more in the direction of interrest because of boundary conditions
+    q_x = np.zeros((phi_mat.shape[0]+1, phi_mat.shape[1]))
+    q_y = np.zeros((phi_mat.shape[0], phi_mat.shape[1]+1))
+    # derive each q_x - don't update boundary cells: keep zero flux
+    for i in range(0, q_x.shape[0]-1):
+        for j in range(1, q_x.shape[1]):
+            rho_mat[i,j]
+            t1 = (rho_mat[i, j-1] + rho_mat[i,j])/2
+            t2 = Kappa((phi_mat[i,j-1]+phi_mat[i,j])/2)
+            t3 = (rho_mat[i,j]-rho_mat[i,j-1])/dx
+            q_x[i,j] = t1 * t2 * t3
+
+    # derive each q_y
+    for i in range(1, q_y.shape[0]):
+        for j in range(0, q_y.shape[1]-1):
+            q_y[i,j] = (rho_mat[i-1, j]+rho_mat[i,j])/2 * Kappa((phi_mat[i-1, j]+phi_mat[i-1, j])/2) * (rho_mat[i,j] - rho_mat[i-1,j])/dy
+
+    # update q_x and q_y based on applied forcing
+    q_x = q_x + add_qx_mat
+    q_y = q_y + add_qy_mat
+
+    # calculate total flux into each grid cell
+    tot_flux = np.zeros(rho_mat.shape)
+    for i in range(tot_flux.shape[0]-1):
+        for j in range(tot_flux.shape[1]-1):
+            x_comp = (q_x[i,j+1]-q_x[i,j])/dx
+            y_comp = (q_y[i+1,j]-q_y[i,j])/dy
+            if (x_comp !=0) or (y_comp !=0):
+                print(i,j, x_comp, y_comp)
+            tot_flux[i,j] = -(x_comp + y_comp)
+    
+    # update rho with the new flux
+    rho_new = rho_mat+ tot_flux
+
+    return rho_new
+
+plt.imshow(update_rho(phi0, rho0, Kappa, additive_q, np.zeros((phi0.shape[0], phi0.shape[1]+1))))
+plt.show()
+
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 from math import tanh
+from numba.core.types.abstract import Number
 import numpy as np
 from numba import jit
+from math import pi
 t_step =0.05
+eps = 0.05
+res=200
+dim = int(np.ceil(res*(1+2*eps)))
 
 
 # ramp up 2D - push water in and suck it out
-def fluid_forcing(pshape, t, scale, len_force=100, start=500):
+def fluid_forcing_x(pshape, t, scale, len_force=100, start=500):
     """Takes shape of initial phi matrix and time stamp
         -Returns forcing; tune magnitude of forcing with scale parameter"""
     f_force = np.zeros(pshape)
@@ -23,6 +28,9 @@ def fluid_forcing(pshape, t, scale, len_force=100, start=500):
 def Kappa_phi(phi: np.ndarray) -> np.ndarray:
     one_mat = np.ones(phi.shape)
     return (one_mat - phi)**3/(phi**2)
+
+def Kappa(p: Number) -> Number:
+    return (1-p)**3/(p**2)
 
 # function to update fluid
 def fluid_vel(phi, grad_p):
@@ -55,3 +63,32 @@ def dphi_FOFD(phi, grad_p, dt=t_step):
 
 
 # function to solve PDE
+
+
+
+# initial conditions
+def w_matrix(eps, res):
+    """Defines exponential decay matrix based on grid resolution (res) and communication length eps"""
+    eps_res = 2*int(eps*res) # eps on each side
+    weight_matrix = np.zeros((eps_res, eps_res))
+    i, j = np.floor(weight_matrix.shape[0])/2, np.floor(weight_matrix.shape[0])/2 # index of middle gridpoint
+    # iterate through gridpoints to determine weighting by distance
+    for k in range(weight_matrix.shape[0]):
+        for l in range(weight_matrix.shape[1]):
+            # division (can be adjusted) gives scale of how fast weighting decreases
+            weight_matrix[k,l] = np.exp(- (((i-k)/res)**2+((j-l)/res)**2) /(eps**2/pi)) 
+    return weight_matrix
+weight_matrix = w_matrix(eps, res)
+
+def convolve_matrix(phi, eps, res):
+    """Smoothing function: applies weight matrix to all gridpoints in phi matrix"""
+    eps_res = int(eps*res)
+    smoothed = np.zeros((res, res))
+    for i in range(eps_res, smoothed.shape[0]+eps_res):
+        for j in range(eps_res,smoothed.shape[0]+eps_res):
+            # index out communication area
+            communication_area = phi[i-eps_res:i+eps_res, j-eps_res: j+eps_res]
+            # elementwise sum of weights and communication points
+            smoothed[i-eps_res,j-eps_res] = np.sum(np.multiply(communication_area, weight_matrix))/((eps_res*2+1))**2
+    
+    return smoothed/(np.max(smoothed))
